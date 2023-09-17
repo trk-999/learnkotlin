c9321367068e05528a49a25cd828d776391ee22d
trk-999 <97623216+trk-999@users.noreply.github.com>
2023-09-16 20:44:00 -0400

    Create learnFunction1.kt
    Learning to return values from functions.

---
+++ learnFunction1.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println(birthdayGreeting())
+}
+
+fun birthdayGreeting(): String {
+    val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
