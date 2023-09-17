8cfae9b821e786653b6c352a60a7766e6ef98aa0
trk-999 <97623216+trk-999@users.noreply.github.com>
2023-09-17 19:19:07 -0400

    Updating as I learn and practice Kotlin

---
+++ 1st_practice_problems/compile_error.kt
@@ -0,0 +1,24 @@
+/*
+
+This program prints a message that notifies the user that they received a chat message from a friend.
+
+
+fun main() { 
+    println("New chat message from a friend'}
+}
+
+1.) 	Can you figure out the root cause of the compile errors in this program and fix them?
+2.) 	Does the code use appropriate symbols to indicate the open and close of the string and function argument?
+
+Hint: You can use Kotlin Playground to run the code and view the compilation errors.
+
+After you fix the errors, the program should compile without errors and print this output:
+
+--------------------------------
+New chat message from a friend
+--------------------------------
+
+ */ 
+
+//SOLUTION BELOW
+//--------------
